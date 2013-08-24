089f037381aa823f3f033ce9c04eef51f2c599ae
martind <martin@dekstop.de>
2013-08-24 13:07:41 +0100

    GPL2 headers

--- src/de/dekstop/andrey/play/Voice.java
+++ src/de/dekstop/andrey/play/Voice.java
@@ -1,3 +1,18 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package de.dekstop.andrey.play;
 
 import de.dekstop.andrey.seq.*;
@@ -26,7 +41,7 @@ public class Voice {
     this.channel = channel;
     this.generator = generator;
   }
-
+  long last = 0;
   public void play(long nowInTicks) {
     // Note off
   	if (curNoteEndInTicks!=-1 && curNoteEndInTicks<=nowInTicks) {
@@ -42,6 +57,9 @@ public class Voice {
     if (note.getPitch() > 0) {
       midiBus.sendNoteOn(channel, note.getPitch(), note.getVelocity());
       System.out.println("Note on: " + note.getPitch() + " at velocity " + note.getVelocity());
+      long t = System.currentTimeMillis();
+//      System.out.println((t - last) + " " + nowInTicks);
+      last = t;
     }
 
     curNote = note;
